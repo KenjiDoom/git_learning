76a54697aba5fbfc7f52247cf45ce2dd4b76dad0
KenjiDoom <vitodevcool@protonmail.com>
2021-01-27 03:41:11 +0000

    some code added

--- server.py
+++ server.py
@@ -1,6 +1,14 @@
+#!/usr/bin/python3
 # new comment added to server
 print ("server is running...")
 print ("I miss python2...")
+
+
+def main():
+    print ("Some code will go here")
+    pass 
+
+
 
 
 
