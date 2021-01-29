6cc37e7e21c07c859b3e22b75a32d8f68c5727e9
KenjiDoom <vitodevcool@protonmail.com>
2021-01-29 05:01:26 +0000

    learning socket programming

--- server.py
+++ server.py
@@ -1,9 +1,41 @@
-# new comment added to server
-print ("server is running...")
-print ("I miss python2...")
+import socket 
+import threading 
 
-def main():
-    print ("This code will be pushed to the master branch.")
-    pass
+HREADER = 64
+PORT = 5050
+# Getting Local IPV4 IP Address 
+SERVER = socket.gethostbyname(socket.gethostname())
+# print(SERVER)
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
 
-main()
+
+#AF_INET = Stands for IPV4 type
+# SOCK_STREAM = streaming data through the socket in the form of bytes 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR) 
+
+def handle_client(conn, addr):
+    print("New Connection {addr} connected.") 
+    
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length.decode(FORMAT)
+        print(f"{addr}, {msg}")
+
+def start():
+    server.listen()
+    while true: 
+        # addr = address bound to the socket on the other end (PORT AND IP) INFO
+        # conn = new socket object usable to send and rev data
+        conn, addr = server.accept()
+        # Runs handle_clinet func
+        thread = threading.Thread(target=handle_client, args=(conn, addr)) 
+        thread.start()
+        print(f"Active Connections {threading.activeCount() - 1}") 
+
+
+print("Sever is starting.")
+start()
